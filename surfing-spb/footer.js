17f1603788cf21992e39380b286fc94308ce2e20
alexanderniki <alexanderniki@iMac-alexanderniki.local>
2021-08-17 22:39:18 +0300

    surf: added header component

---
+++ surfing-spb/footer.js
@@ -0,0 +1,27 @@
+/* 
+ * footer.js 
+ */
+
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <section class="main-footer caption">
+            <p>КОНТАКТЫ:</p>
+            <ul>
+                <li>Telegram/Whatsapp: @alexanderniki</li>
+                <li>Instagram: <a href="https://instagram.com/alexanderniki">@alexanderniki</a></li>
+            </ul>
+        </section>
+        `;
+    }
+}
+
+customElements.define("footer-component", Footer);
